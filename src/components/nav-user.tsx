f10b4afbdf3bb338d77f7099301a8260d55f4412
jaoharil <jaoharilmaknun2904@gmail.com>
2025-04-04 20:14:42 +0800

    edit fitur xp

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -1,153 +1,111 @@
-'use client'
+'use client';
 
-import {
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
-	useSidebar,
-} from './ui/sidebar'
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
-import {
-	ChevronsUpDown,
-	Coins,
-	LogOut,
-	Sparkles,
-	Users,
-} from 'lucide-react'
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuGroup,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-} from './ui/dropdown-menu'
-import { logout } from '@/hooks/useCookies'
-import { useRouter } from 'next/navigation'
-import Link from 'next/link'
+import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from './ui/sidebar';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { ChevronsUpDown, Coins, LogOut, Sparkles, Users } from 'lucide-react';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu';
+import { logout } from '@/hooks/useCookies';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const NavUser = ({
-	user,
+  user,
 }: {
-	user: {
-		name: string
-		email: string
-		avatar: string
-		isPremium: boolean
-	}
+  user: {
+    name: string;
+    email: string;
+    avatar: string;
+    isPremium: boolean;
+  };
 }) => {
-	const { isMobile } = useSidebar()
-	const router = useRouter()
+  const { isMobile } = useSidebar();
+  const router = useRouter();
 
-	return (
-		<SidebarMenu>
-			<SidebarMenuItem>
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<SidebarMenuButton
-							size='lg'
-							className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'>
-							<Avatar
-								className={`h-6 w-6 ${
-									user.isPremium &&
-									'ring-2 ring-blue-700 ring-offset-1'
-								}`}>
-								<AvatarImage
-									src={user.avatar}
-									alt={user.name}
-								/>
-								<AvatarFallback>
-									{user.name
-										.split(' ')
-										.map((word) => word[0])
-										.slice(0, 2)
-										.join('')
-										.toUpperCase()}
-								</AvatarFallback>
-							</Avatar>
-							<div className='grid flex-1 text-left text-sm leading-tight'>
-								<span className='truncate font-semibold'>
-									{user.name}
-								</span>
-								<span className='truncate text-xs'>{user.email}</span>
-							</div>
-							<ChevronsUpDown className='ml-auto size-4' />
-						</SidebarMenuButton>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent
-						className='w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg'
-						side={isMobile ? 'bottom' : 'right'}
-						align='end'
-						sideOffset={4}>
-						<DropdownMenuLabel className='p-0 font-normal'>
-							<div className='flex items-center gap-2 px-1 py-1.5 text-left text-sm'>
-								<Avatar className='h-8 w-8'>
-									<AvatarImage
-										src={user.avatar}
-										alt={user.name}
-									/>
-									<AvatarFallback>
-										{user.name
-											.split(' ')
-											.map((word) => word[0])
-											.slice(0, 2)
-											.join('')
-											.toUpperCase()}
-									</AvatarFallback>
-								</Avatar>
-								<div className='grid flex-1 text-left text-sm leading-tight'>
-									<span className='truncate font-semibold'>
-										{user.name}
-									</span>
-									<span className='truncate text-xs'>
-										{user.email}
-									</span>
-								</div>
-							</div>
-						</DropdownMenuLabel>
-						<DropdownMenuSeparator />
-						{user.isPremium && (
-							<>
-								<DropdownMenuGroup>
-									<DropdownMenuItem
-										disabled
-										className='!opacity-100'>
-										<Coins />
-										1200 XP
-									</DropdownMenuItem>
-								</DropdownMenuGroup>
-								<DropdownMenuSeparator />
-							</>
-						)}
-						<DropdownMenuGroup>
-							{!user.isPremium && (
-								<DropdownMenuItem asChild>
-									<Link href='https://academy.fysite.id'>
-										<Sparkles />
-										Upgrade to Pro
-									</Link>
-								</DropdownMenuItem>
-							)}
-							<DropdownMenuItem>
-								<Users />
-								Community
-							</DropdownMenuItem>
-						</DropdownMenuGroup>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem
-							onClick={() => {
-								logout()
-								router.refresh()
-							}}>
-							<LogOut />
-							Log out
-						</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
-			</SidebarMenuItem>
-		</SidebarMenu>
-	)
-}
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton size="lg" className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
+              <Avatar className={`h-6 w-6 ${user.isPremium && 'ring-2 ring-blue-700 ring-offset-1'}`}>
+                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarFallback>
+                  {user.name
+                    .split(' ')
+                    .map((word) => word[0])
+                    .slice(0, 2)
+                    .join('')
+                    .toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">{user.name}</span>
+                <span className="truncate text-xs">{user.email}</span>
+              </div>
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg" side={isMobile ? 'bottom' : 'right'} align="end" sideOffset={4}>
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="h-8 w-8">
+                  <AvatarImage src={user.avatar} alt={user.name} />
+                  <AvatarFallback>
+                    {user.name
+                      .split(' ')
+                      .map((word) => word[0])
+                      .slice(0, 2)
+                      .join('')
+                      .toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">{user.name}</span>
+                  <span className="truncate text-xs">{user.email}</span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            {/* xp */}
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem disabled className="!opacity-100">
+                <Coins />
+                {user.isPremium ? '1200 XP' : '0 XP'}
+                {/* {user.isPremium ? `${user.xp} XP` : '0 XP'} */}
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
 
-export default NavUser
+            <DropdownMenuSeparator />
+            {/* end xp */}
+            <DropdownMenuGroup>
+              {!user.isPremium && (
+                <DropdownMenuItem asChild>
+                  <Link href="https://academy.fysite.id">
+                    <Sparkles />
+                    Upgrade to Pro
+                  </Link>
+                </DropdownMenuItem>
+              )}
+              <DropdownMenuItem>
+                <Users />
+                Community
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={() => {
+                logout();
+                router.refresh();
+              }}
+            >
+              <LogOut />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+};
+
+export default NavUser;
--- src/config/firebase/index.ts
+++ src/config/firebase/index.ts
@@ -1,34 +1,30 @@
-import { initializeApp } from 'firebase/app'
-import {
-	getAuth,
-	GoogleAuthProvider,
-	signInWithPopup,
-} from 'firebase/auth'
-import { getDatabase } from 'firebase/database'
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId:
-		process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-}
+  apiKey: 'AIzaSyDfEIEslcYbaVQU-jLgLQivroPWt9LvHMk',
+  authDomain: 'fysite-2c797.firebaseapp.com',
+  databaseURL: 'https://fysite-2c797-default-rtdb.firebaseio.com',
+  projectId: 'fysite-2c797',
+  storageBucket: 'fysite-2c797.firebasestorage.app',
+  messagingSenderId: '872690344126',
+  appId: '1:872690344126:web:eb8c2075ebffabf145feef',
+  measurementId: 'G-9145D1X5X5',
+};
 
-const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export const db = getDatabase(app) // **Pastikan ini diekspor**
-export const googleProvider = new GoogleAuthProvider()
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app); // **Pastikan ini diekspor**
+export const googleProvider = new GoogleAuthProvider();
 
 // 🔹 Fungsi Login dengan Google
 export const signInWithGoogle = async () => {
-	try {
-		const result = await signInWithPopup(auth, googleProvider)
-		return result.user
-	} catch (error) {
-		console.error('Login failed', error)
-		throw error
-	}
-}
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    return result.user;
+  } catch (error) {
+    console.error('Login failed', error);
+    throw error;
+  }
+};
