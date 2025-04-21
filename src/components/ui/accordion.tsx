15eeacf7f72c56ce0df67f64ed23b9c3686b8a68
withme <168322939+devswithme@users.noreply.github.com>
2025-04-21 13:07:37 +0800

    Update accordion.tsx

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -28,7 +28,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-6 font-medium transition-all hover:underline text-left [&[data-state=open]>svg]:rotate-45",
+        "flex flex-1 items-center justify-between py-6 text-sm sm:text-base font-medium transition-all hover:underline text-left [&[data-state=open]>svg]:rotate-45",
         className
       )}
       {...props}
@@ -46,7 +46,7 @@ const AccordionContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="overflow-hidden data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+    className="overflow-hidden data-[state=closed]:animate-accordion-up text-sm sm:text-base data-[state=open]:animate-accordion-down"
     {...props}
   >
     <div className={cn("pb-4 pt-0", className)}>{children}</div>
