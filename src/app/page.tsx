9a782c894b1cd50d40e742d12e4895e950bc4b3b
devswithme <developswithme@gmail.com>
2025-05-21 07:32:27 +0800

    add og metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,29 @@ export const metadata: Metadata = {
   title: "Fydemy",
   description:
     "A web development academy platform empowering the next generation of digital innovators",
+  metadataBase: new URL("https://edu.fysite.id"),
+  openGraph: {
+    title: "Fydemy",
+    description:
+      "A web development academy platform empowering the next generation of digital innovators",
+    url: "https://edu.fysite.id",
+    type: "website",
+    images: [
+      {
+        url: "/og.png",
+        width: 1440,
+        height: 812,
+        alt: "fydemy",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Fydemy",
+    description:
+      "A web development academy platform empowering the next generation of digital innovators",
+    images: ["/og.png"],
+  },
 };
 
 export default async function Page() {
